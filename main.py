4b277c5318ebb0d496e25a3e313009d9cb5923e6
NashVillarta <>
2021-05-09 21:57:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This inputs math into the python code so that math.pi could function.
+import math
+
+# This part calculates the circumference. Just like the JS code, it reads the input and puts it into the variable and runs it through the string and then displaying on the console.
+print("Please input radius for Circumference")
+radiusC = int(input())
+print (2 * math.pi * radiusC)
+# Like before, it takes the input and runs it through the string and then prints displaying the area.
+print("Please input radius for Area")
+radiusA = int(input())
+print (math.pi * radiusA * radiusA)
